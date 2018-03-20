b6da73b851f6b612024697425cb32579a6fb8cab
Sherill Samuel <sherill.samuel@gmail.com>
2018-03-19 20:54:20 -0400

    first commit

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+(function( $ ){
+
+
+
+   	/* ----------------------------------------------------------- */
+	/*  3. TESTIMONIALS (SLICK SLIDER)
+	/* ----------------------------------------------------------- */
+
+		$('.mu-testimonial-slide').slick({
+			arrows: true,
+			dots: false,
+			infinite: true,
+			speed: 500,
+			autoplay: true,
+			cssEase: 'linear'
+		});
+
+})( jQuery );
